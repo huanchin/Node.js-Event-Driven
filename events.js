e68cee709990241eb99d1f74fbb26a42759d8bcd
huanchin <cherry032826@gmail.com>
2023-10-23 07:13:48 +0800

    Update events.js

--- events.js
+++ events.js
@@ -3,29 +3,29 @@ const http = require("http");
 
 /************ this example shows how to use Event-Driven Architecture ****************/
 
-// const myEmitter = new EventEmitter();
+const myEmitter = new EventEmitter();
 
-// // set up newSale event listener
-// // this one here(.on()) is the observer, it observe the emitter and wait until it emits the newSale event
-// myEmitter.on("newSale", () => {
-//   console.log("There was a new sale!");
-// });
+// set up newSale event listener
+// this one here(.on()) is the observer, it observe the emitter and wait until it emits the newSale event
+myEmitter.on("newSale", () => {
+  console.log("There was a new sale!");
+});
 
-// // we can set up multiple event listener for the same event
-// // this one here(.on()) is the observer, it observe the emitter and wait until it emits the newSale event
-// myEmitter.on("newSale", () => {
-//   console.log("Costumer name: Jonas");
-// });
+// we can set up multiple event listener for the same event
+// this one here(.on()) is the observer, it observe the emitter and wait until it emits the newSale event
+myEmitter.on("newSale", () => {
+  console.log("Costumer name: Jonas");
+});
 
-// // this event listener takes the argument in emitter as an argument of its callback function
-// myEmitter.on("newSale", (stock) => {
-//   console.log(`There are now ${stock} items left in stock.`);
-// });
+// this event listener takes the argument in emitter as an argument of its callback function
+myEmitter.on("newSale", (stock) => {
+  console.log(`There are now ${stock} items left in stock.`);
+});
 
-// // make up an event name
-// // this one here(.emit()) is the object that emits the events
-// // we can even pass arguments to the EventListener by passing them as an additional arguments in the emitter
-// myEmitter.emit("newSale", 9);
+// make up an event name
+// this one here(.emit()) is the object that emits the events
+// we can even pass arguments to the EventListener by passing them as an additional arguments in the emitter
+myEmitter.emit("newSale", 9);
 
 /*********** to use Oberver Pattern in real life, its better to creat a new class that will actually inherit from node EventListener *********/
 class Sales extends EventEmitter {
